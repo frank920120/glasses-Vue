eeebf5b71df87c056be289ff11edd992bc3356a2
Frank Fang <frankfang2014@hotmail.com>
2019-12-12 16:11:02 -0800

    add vant

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <div id="app">
     <NavView></NavView>
     <router-view></router-view>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
     <TabView></TabView>
   </div>
 </template>
@@ -14,13 +9,11 @@
 <script>
 import NavView from "./components/NavView/NavView.vue";
 import TabView from "./components/TabView/TabView.vue";
-import { Button } from "vant";
 export default {
   name: "app",
   components: {
     NavView,
-    TabView,
-    [Button.name]: Button
+    TabView
   }
 };
 </script>
--- src/components/TabView/TabView.vue
+++ src/components/TabView/TabView.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="tabbar-view">
-    <router-link to="/home">首页</router-link>
+  <van-tabbar v-model="active" class="tabbar-view">
+    <router-link to="/home"
+      ><van-tabbar-item icon="home-o">首页</van-tabbar-item></router-link
+    >
+    <router-link to="/sort"
+      ><van-tabbar-item icon="search">分类</van-tabbar-item></router-link
+    >
+    <router-link to="/search">
+      <van-tabbar-item icon="friends-o">搜索</van-tabbar-item></router-link
+    >
+    <router-link to="/cart"
+      ><van-tabbar-item icon="setting-o">购物车</van-tabbar-item></router-link
+    >
+    <router-link to="/my">
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item></router-link
+    >
+  </van-tabbar>
+  <!-- <router-link to="/home">首页</router-link>
     <router-link to="/sort">分类</router-link>
     <router-link to="/search">搜索</router-link>
     <router-link to="/cart">购物车</router-link>
-    <router-link to="/my">我的</router-link>
-  </div>
+    <router-link to="/my">我的</router-link> -->
 </template>
 
 <script>
-export default {};
+import { Tabbar, TabbarItem } from "vant";
+export default {
+  data() {
+    return {
+      active: 0
+    };
+  },
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  }
+};
 </script>
 
 <style scoped>
@@ -19,6 +45,8 @@ export default {};
   bottom: 0;
   border-top: 0.0625rem solid black;
   width: 100%;
+  display: flex;
+  align-items: center;
 }
 .tabbar-view a {
   display: inline-block;
