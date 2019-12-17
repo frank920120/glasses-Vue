d489372a5be1ac3ee0667d1bf666ebe2c1f92993
Frank Fang <frankfang2014@hotmail.com>
2019-12-16 16:50:41 -0800

    add price section to the detail component

--- src/components/ItemDetailView/ItemDetailView.vue
+++ src/components/ItemDetailView/ItemDetailView.vue
@@ -6,6 +6,16 @@
         <img v-lazy="image.img" class="banner" />
       </van-swipe-item>
     </van-swipe>
+    <div class="content-view">
+      <div class="price-view">
+        <p class="new-price">¥ {{ details.newPrice }}</p>
+        <p class="old-price">原价 ¥ {{ details.oldPrice }}</p>
+        <span class="sale-item" v-if="details.sale">特价</span>
+      </div>
+      <div class="standard-view"></div>
+      <div class="desc-view"></div>
+      <div class="explain-view"></div>
+    </div>
   </div>
 </template>
 
@@ -43,7 +53,7 @@ export default {
 
 <style scoped>
 .itemdetail-view {
-  background: blue;
+  background: #f2f2f2;
   position: fixed;
   top: 0;
   left: 0;
@@ -53,5 +63,40 @@ export default {
 }
 .banner {
   width: 100%;
+}
+.content-view {
+  background: white;
+  border: 1px solid #bbbbbb;
+  border-left: 0;
+  border-right: 0;
+}
+.price-view {
+  height: 2.8125rem;
+  line-height: 2.8125rem;
+}
+.new-price {
+  float: left;
+  font-size: 1.3125rem;
+  font-weight: 900;
+  margin-right: 1.25rem;
+  margin-left: 1.25rem;
+  color: red;
+}
+.old-price {
+  font-size: 0.75rem;
+  color: #7c7c7c;
+  float: left;
+  margin-right: 1.25rem;
+  text-decoration: line-through;
+}
+.sale-item {
+  display: inline-block;
+  font-size: 0.75rem;
+  color: white;
+  background: red;
+  width: 1.875rem;
+  height: 1.125rem;
+  line-height: 1.125rem;
+  text-align: center;
 }
 </style>
