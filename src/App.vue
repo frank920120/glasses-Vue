e41de4e711da53955b54339c65d81d770b1cce59
Frank Fang <frankfang2014@hotmail.com>
2019-12-16 16:08:13 -0800

    add nav and swipe to the detail component

--- src/App.vue
+++ src/App.vue
@@ -37,4 +37,20 @@ export default {
 #app {
   background-color: #f2f2f2;
 }
+.van-nav-bar {
+  height: 2.8125rem;
+  background-color: red;
+  line-height: 2.8125rem;
+  text-align: center;
+}
+.van-nav-bar .van-icon {
+  color: white;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.van-nav-bar__title {
+  color: white;
+  font-size: 1rem;
+  font-weight: 700;
+}
 </style>
--- src/components/ItemDetailView/ItemDetailView.vue
+++ src/components/ItemDetailView/ItemDetailView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="itemdetail-view" @click="goback">detail-{{ getId }}</div>
+  <div class="itemdetail-view">
+    <van-nav-bar title="商品详情" left-arrow @click-left="goback" />
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="image in details.banners" :key="image.id">
+        <img v-lazy="image.img" class="banner" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 
 <script>
+import { NavBar, Swipe, SwipeItem } from "vant";
 export default {
+  data() {
+    return {
+      details: []
+    };
+  },
+  components: {
+    [NavBar.name]: NavBar,
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem
+  },
   computed: {
     getId() {
       return this.$store.state.itemId;
@@ -13,6 +31,12 @@ export default {
     goback: function() {
       this.$store.commit("changeItemDetailsShow");
     }
+  },
+  created() {
+    this.axios
+      .get("/json/goods-detail.json")
+      .then(res => (this.details = res.data))
+      .catch(err => console.log(err));
   }
 };
 </script>
@@ -26,5 +50,8 @@ export default {
   height: 100%;
   width: 100%;
   z-index: 999;
+}
+.banner {
+  width: 100%;
 }
 </style>
